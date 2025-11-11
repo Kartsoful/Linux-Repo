482b19d5d628e679ef7cabdb4a58e0b3e5a0a4f2
Etunimesi Sukunimesi <shkpostisi@example.com>
2025-11-11 13:09:59 +0200

    Uusi script

--- script.sh
+++ script.sh
@@ -1,13 +1,12 @@
-!/bin/bash
+#!/bin/bash
 
 # Scriptillä ladataan git repo ja siirretään repon index.html tiedosto
 # juureen, mistä Nginx-hakee staattisen html-tiedot
-# siirrä tämä juureen /home/<oma_tunnus>/<repo-kansio>/
 
 # vaihda tähän kotikansiosi
-cd /home/<oma_tunnus>/<repo-kansio>/
+cd /home/ubuntu/Linux-Repo/
 
 git pull
 
 # siirtää tiedoston juureen, josta Nginx hakee staattisen html-tiedoston
-sudo mv ~/<repo-kansio>/index.html /var/www/html/index.html
+sudo mv ~/Linux-Repo/index.html /var/www/html/index.html
