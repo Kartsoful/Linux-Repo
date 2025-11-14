36770ee3b428ce447c0af3fd6f3d1ef100155462
Etunimesi Sukunimesi <shkpostisi@example.com>
2025-11-14 20:59:35 +0200

    .py

--- streamlit_test.py
+++ streamlit_test.py
@@ -4,16 +4,8 @@ import plotly.express as px
 
 def main():
     st.title("Electric price - Test")
-
     df = pd.read_csv("Electric_prices.csv", sep=",")
-
-    ff = px.line(
-        df,
-        x="Date",
-        y=["Paaoma", "2%", "4%", "6%"],   # useampi sarake listana
-        markers=True
-    )
-
+    ff = px.scatter(df, x="Date", y="Paaoma")
     st.plotly_chart(ff, use_container_width=True)
 
 if __name__ == "__main__":
