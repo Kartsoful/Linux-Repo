07dd3899b2d8432272c11686d97f59bb1fe0ec43
Etunimesi Sukunimesi <shkpostisi@example.com>
2025-11-14 17:13:53 +0200

    .

--- script.sh
+++ script.sh
@@ -7,11 +7,11 @@ cd /home/ubuntu/Linux-Repo/
 git pull
 
 # siirtää päivitetyt tiedoston oikeisiin paikkoihin
-sudo mv ~/Linux-Repo/index.html /var/www/html/index.html
-sudo mv ~/Linux-Repo/LH2.gif /var/www/html/LH2.gif
-sudo mv ~/Linux-Repo/app.py ~/lemp-app/app.py
-sudo mv ~/Linux-Repo/lemp-app /etc/nginx/sites-available/lemp-app
-sudo mv ~/Linux-Repo/lemp-app.service /etc/systemd/system/lemp-app.service
+sudo cp ~/Linux-Repo/index.html /var/www/html/index.html
+sudo cp ~/Linux-Repo/LH2.gif /var/www/html/LH2.gif
+sudo cp ~/Linux-Repo/app.py ~/lemp-app/app.py
+sudo cp ~/Linux-Repo/lemp-app /etc/nginx/sites-available/lemp-app
+sudo cp ~/Linux-Repo/lemp-app.service /etc/systemd/system/lemp-app.service
 
 # Startataan service uudelleen
 sudo systemctl restart lemp-app
