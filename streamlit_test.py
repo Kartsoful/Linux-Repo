0bd6b11f3ef4e42af52293a6ae2e942d05ad3e9e
Etunimesi Sukunimesi <shkpostisi@example.com>
2025-11-14 19:44:55 +0200

    .

--- streamlit_test.py
+++ streamlit_test.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 def main():
-    st.title("Plot some data")
+    st.title("Electric price - Test")
     df = pd.read_csv("Electric_prices.csv", sep=",")
     ff = px.scatter(df, x="Date", y="Price")
     st.plotly_chart(ff, use_container_width=True)
