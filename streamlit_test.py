ae19f4d1dd7cd18725753056cd116fc7f0addd1c
Etunimesi Sukunimesi <shkpostisi@example.com>
2025-11-14 22:18:48 +0200

    .py

--- streamlit_test.py
+++ streamlit_test.py
@@ -1,31 +1,57 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+@st.cache_resource
+def mySql():
+
+    # Initialize connection.
+    conn = st.connection('mysql', type='sql')
+    # Perform query.
+    df = conn.query('SELECT Oulu from lampotilat;', ttl=600)
+    return df
+# Streamlit
+
 
 def main():
-    st.title("Electric price - Test")
+    st.title("Plot data from MySql")
+    st.write("Lämpötiloja ")
+    data = mySql()
+    #plot data
 
-    # Lue data
-    df = pd.read_csv("Electric_prices.csv", sep=",")
+    options = st.multiselect(
+    "Minkä kaupungin lämpötilat?",
+    ["Utsjoki", "Oulu", "Helsinki"],
+    default=["Oulu"],
+)
 
-    # Siivoa otsikot: poista mahdolliset ; ja välilyönnit alusta/lopusta
-    df.columns = [c.strip().replace(";", "") for c in df.columns]
-
-    # Näytä debugina mitä sarakkeita oikeasti on
-    st.write("Sarakkeet:", list(df.columns))
-
-    # Oletus: Date = x-akseli, kaikki muut sarakkeet (esim. Paaoma, 2%, 4%, 6%) = viivat
-    y_cols = [c for c in df.columns if c != "Date"]
-
-    fig = px.line(
-        df,
-        x="Date",
-        y=y_cols,
-        markers=True,
-        title="Trendiviivat"
-    )
-
-    st.plotly_chart(fig, use_container_width=True)
+    df2 = pd.DataFrame(data, columns=["Päivittäinen lämpötila"])
+    lampotila = px.line(df2, x=df2.index, y=options)
+    st.plotly_chart(lampotila, use_container_width=True)
 
 if __name__ == "__main__":
     main()
+
+
+
+# import streamlit as st
+# import pandas as pd
+# import plotly.express as px
+# @st.cache_resource
+# def mySql():
+
+#     # Initialize connection.
+#     conn = st.connection('mysql', type='sql')
+#     # Perform query.
+#     df = conn.query('SELECT TotalKg from powerlifting_28_12_24 LIMIT 100;', ttl=600)
+#     return df
+# # Streamlit
+# def main():
+#     st.title("Plot data from MySql")
+#     st.write("TotalKg from powerlifting")
+#     data = mySql()
+#     #plot data
+#     df2 = pd.DataFrame(data, columns=["TotalKg"])
+#     totalKg = px.line(df2, x=df2.index, y="TotalKg")
+#     st.plotly_chart(totalKg, use_container_width=True)
+# if __name__ == "__main__":
+#     main()
