48dbbbaf5b407196839653b218cc1a1d8d6dc1d2
Etunimesi Sukunimesi <shkpostisi@example.com>
2025-11-14 17:11:58 +0200

    app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ app = Flask(__name__)
 def db():
     return mysql.connector.connect(
         host="127.0.0.1",
-        user="user",          # vaihda omiin
-        password="password",   # vaihda omiin
-        database="database"    # vaihda omiin
+        user="kartso",          # vaihda omiin
+        password="kartso123",   # vaihda omiin
+        database="exampledb"    # vaihda omiin
     )
 
 # --- etusivu ---
