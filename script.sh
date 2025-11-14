1f510db98c949bc3c8ad3d31b717177e8de5cbfe
Etunimesi Sukunimesi <shkpostisi@example.com>
2025-11-14 17:09:03 +0200

    .

--- script.sh
+++ script.sh
@@ -8,6 +8,7 @@ git pull
 
 # siirtää päivitetyt tiedoston oikeisiin paikkoihin
 sudo mv ~/Linux-Repo/index.html /var/www/html/index.html
+sudo mv ~/Linux-Repo/LH2.gif /var/www/html/LH2.gif
 sudo mv ~/Linux-Repo/app.py ~/lemp-app/app.py
 sudo mv ~/Linux-Repo/lemp-app /etc/nginx/sites-available/lemp-app
 sudo mv ~/Linux-Repo/lemp-app.service /etc/systemd/system/lemp-app.service
