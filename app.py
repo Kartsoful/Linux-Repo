6409f266a9512bcebcfb60ad58db7f8e7ebe4703
Etunimesi Sukunimesi <shkpostisi@example.com>
2025-11-15 22:46:14 +0200

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # Toimiva app.py tiedosto
 # Palauttaa SQL-palvelimen ajan ja tallentaa Speden pelin pisteet MySQL-tietokantaan
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 import mysql.connector
 
 app = Flask(__name__)
@@ -10,20 +10,10 @@ app = Flask(__name__)
 def db():
     return mysql.connector.connect(
         host="127.0.0.1",
-        user="user",          # vaihda omiin
-        password="pass",   # vaihda omiin
-        database="db"    # vaihda omiin
+        user="kartso",          # vaihda omiin
+        password="kartso123",   # vaihda omiin
+        database="exampledb"    # vaihda omiin
     )
-
-# --- etusivu ---
-@app.route("/")
-def home():
-    conn = db()
-    cur = conn.cursor()
-    cur.execute("SELECT NOW()")
-    ts = cur.fetchone()[0]
-    cur.close(); conn.close()
-    return f"<h1>Hello from MySQL!</h1><p>SQL-palvelimen aika: <b>{ts}</b></p>"
 
 # --- kellonajan API ---
 @app.route("/time")
@@ -35,5 +25,7 @@ def time_api():
     cur.close(); conn.close()
     return jsonify({"time": str(ts)})
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
 
