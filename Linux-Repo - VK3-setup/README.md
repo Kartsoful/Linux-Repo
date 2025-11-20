894e9ec1b026c0ac7332de9a68cd02de5caad0f0
Etunimesi Sukunimesi <shkpostisi@example.com>
2025-11-20 16:23:39 +0200

    VK3-harjoituksen backup

---
+++ Linux-Repo - VK3-setup/README.md
@@ -0,0 +1,10 @@
+Tämä repositori sisältää tiedostoja, joita on käytetty linuxin LEMP-stack harjoituksiin.
+Sisältö on nyt enemmän VK3-harjoituksen mukainen.
+
+Kansiosta "Linux-Reop - VK2-setup" löytyy enemmän VK2-harjoitukseen liittyvää tietoa. Sielläkin on osin jo VK3-harjoituksen tietoa.
+
+Linkki VK3 harjoitukseen:
+http://86.50.20.94/
+
+Linkki VK4 harjoitukseen - Harjoitus on keskeneräinen:
+http://86.50.20.24/
---
+++ Linux-Repo - VK3-setup/script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Scriptillä ladataan git repo ja siirretään
+# päivitetyt tiedostot niille kuuluville sijainneille
+
+cd /home/ubuntu/Linux-Repo/
+git pull
+
+# siirtää päivitetyt tiedoston oikeisiin paikkoihin
+sudo cp ~/Linux-Repo/index.html /var/www/html/index.html
+sudo cp ~/Linux-Repo/LH2.gif /var/www/html/LH2.gif
+sudo cp ~/Linux-Repo/app.py ~/lemp-app/app.py
+sudo cp ~/Linux-Repo/lemp-app /etc/nginx/sites-available/lemp-app
+sudo cp ~/Linux-Repo/lemp-app.service /etc/systemd/system/lemp-app.service
+sudo cp ~/Linux-Repo/streamlit_test.py /home/ubuntu/lemp-app/streamlit_test.py
+sudo cp ~/Linux-Repo/Electric_prices.csv /home/ubuntu/lemp-app/Electric_prices.csv
+sudo cp ~/Linux-Repo/streamlit.service /etc/systemd/system/streamlit.service
+
+# Startataan service uudelleen
+sudo systemctl daemon-reload
+sudo systemctl reload nginx
+sudo systemctl restart lemp-app
+
+
