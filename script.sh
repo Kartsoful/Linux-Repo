d9df46c34ea865d57f62c304006ba3bb1ec1065f
Kartsoful <kartsoful@gmail.com>
2025-12-08 02:02:02 +0200

    Update

--- script.sh
+++ script.sh
@@ -7,22 +7,23 @@ git pull
 
 # siirtää päivitetyt tiedoston oikeisiin paikkoihin
 # Tuli tehtyä samalla myös docker-LEMP-kontti
-sudo cp ~/Linux-Repo/backend/requirements.txt /home/ubuntu/lemp-containers/backend/requirements.txt
-sudo cp ~/Linux-Repo/backend/Dockerfile /home/ubuntu/lemp-containers/backend/Dockerfile
-sudo cp ~/Linux-Repo/frontend/index.html /home/ubuntu/lemp-containers/frontend/index.html
-sudo cp ~/Linux-Repo/frontend/nginx.conf /home/ubuntu/lemp-containers/frontend/nginx.conf
-sudo cp ~/Linux-Repo/frontend/Dockerfile /home/ubuntu/lemp-containers/frontend/Dockerfile
+sudo cp ~/Linux-Repo/docker_backend/requirements.txt /home/ubuntu/lemp-containers/backend/requirements.txt
+sudo cp ~/Linux-Repo/docker_backend/Dockerfile /home/ubuntu/lemp-containers/backend/Dockerfile
+sudo cp ~/Linux-Repo/docker_frontend/index.html /home/ubuntu/lemp-containers/frontend/index.html
+sudo cp ~/Linux-Repo/docker_frontend/nginx.conf /home/ubuntu/lemp-containers/frontend/nginx.conf
+sudo cp ~/Linux-Repo/docker_frontend/Dockerfile /home/ubuntu/lemp-containers/frontend/Dockerfile
 sudo cp ~/Linux-Repo/docker-compose.dev.yml /home/ubuntu/lemp-containers/docker-compose.dev.yml
 sudo cp ~/Linux-Repo/docker-compose.prod.yml /home/ubuntu/lemp-containers/docker-compose.prod.yml
 
 # Anonymisoidut
-# sudo cp ~/Linux-Repo/db/init/init.sql /home/ubuntu/lemp-containers/db/init/init.sql
-# sudo cp ~/Linux-Repo/backend/app.py /home/ubuntu/lemp-containers/backend/app.py
+# sudo cp ~/Linux-Repo/docker_db/init/init.sql /home/ubuntu/lemp-containers/db/init/init.sql
+# sudo cp ~/Linux-Repo/docker_backend/app.py /home/ubuntu/lemp-containers/backend/app.py
 
 
 
 # Varsinainen kubernetes
 sudo cp ~/Linux-Repo/mysql-deployment.yaml /home/ubuntu/kube/mysql-deployment.yaml
+sudo cp ~/Linux-Repo/mysql-pvc.yaml /home/ubuntu/kube/mysql-pvc.yaml
 
 # Anonymisoidut
 # sudo cp ~/Linux-Repo/mysql-secret.yaml /home/ubuntu/kube/mysql-secret.yaml
